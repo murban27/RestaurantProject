a430a9a724db4a949f7a675a0ba6b4fc9f700618
Urban, Michal <Michal.Urban@tesco.com>
2020-06-30 03:20:07 +0200

    Will continue tomorrow

--- App2/Views/Login.xaml.cs
+++ App2/Views/Login.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using App2.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +14,12 @@ namespace App2.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Login : ContentPage
     {
+        public LoginViewModel viewModel;
         public Login()
         {
             InitializeComponent();
+           
+            BindingContext = viewModel = new LoginViewModel();
         }
     }
 }
