373f19b49c2ce52d40a9c3cb6816f81afa7f17e0
Urban, Michal <Michal.Urban@tesco.com>
2020-06-30 13:15:26 +0200

    images

--- App2/Views/Login.xaml.cs
+++ App2/Views/Login.xaml.cs
@@ -20,7 +20,7 @@ namespace App2.Views
             InitializeComponent();
            
             BindingContext = viewModel = new LoginViewModel();
-            dataForm.BindingContext = viewModel.Login;
+           
 
 
         }
